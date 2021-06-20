45762e895ee2faf2cf58a000b3056f6c03261a2e
Tamara Nikitina <oxian66@mail.ru>
2021-06-20 12:25:52 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,7 @@ program
 .version('0.1')
 .description('Compares two configuration files and shows a difference.')
 .option('-f, --format [type]', 'output format')
+.arguments('<filepath1> <filepath2>')
 
 program.parse();
 
